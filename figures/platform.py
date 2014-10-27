818d2ccc1c68310b5f3424d5e13d327fab1f4cd4
Marshall Ward <marshall.ward@gmail.com>
2014-10-27 23:04:10 +1100

    Fix 12ppn values

--- figures/platform.py
+++ figures/platform.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
 import yaml
 
+f_raijin = 2601e6
+f_fujin = 1848e6
+
 pdata_files = ['raijin.yaml',
                'ht.yaml',
                '12ppn.yaml',
                'ladder.yaml',
                'snake.yaml']
 
+platforms = [p.rstrip('.yaml') for p in pdata_files]
+
 prof_data = {}
-for fname in pdata_files:
+for plat, fname in zip(platforms, pdata_files):
     with open(fname) as pfile:
-        prof_data[fname] = yaml.load(pfile)
+        prof_data[plat] = yaml.load(pfile)
 
-wtime = []
-for platform in prof_data:
-    wtime.append(prof_data[platform][480]['runtime']['total'])
+wtime = {}
+for plat in platforms:
+    wtime[plat] = prof_data[plat][480]['runtime']['total']
 
-print(wtime)
+wtimes = [wtime[p] for p in platforms]
+cycles = [f_raijin * wtime[p] for p in platforms[:3]] \
+        + [f_fujin * wtime[p] for p in platforms[3:]]
+
+for i in range(5):
+    print(platforms[i], cycles[i] * 1e-9)
