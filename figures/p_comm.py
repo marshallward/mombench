5eaa40e087fb33a149f0154f24292ab1c9ca1a37
Marshall Ward <marshall.ward@gmail.com>
2014-10-30 16:49:24 +1100

    New data and new plots

--- figures/p_comm.py
+++ figures/p_comm.py
@@ -20,33 +20,32 @@ prof_data['12ppn'].pop(3840)
 
 ncpus = {}
 wtime = {}
-speedup = {}    # Unused
 effcy = {}
 pcomm = {}
 
 for p in prof_data:
     pdata = prof_data[p]
-    wdata = sorted([(n, pdata[n]['runtime']['total']) for n in pdata])
-    cdata = sorted([(n, pdata[n]['mpi']['mean']) for n in pdata])
-    ncpus_raw, wtime_raw = zip(*wdata)
-    _, pcomm_raw = zip(*cdata)
+    comm_data = sorted([(n, pdata[n]['mpi']['mean']) for n in pdata])
+    msend_data = sorted([(n, pdata[n]['mpi']['wait']['time']) for n in pdata])
+    mreduce_data = sorted([(n, pdata[n]['mpi']['reduce']['time']) for n in pdata])
+    ncpus_raw, pcomm_raw = zip(*comm_data)
+    _, msend_raw = zip(*msend_data)
+    _, mreduce_raw = zip(*mreduce_data)
 
     ncpus[p] = np.array(ncpus_raw)
-    wtime[p] = np.array(wtime_raw)
-    speedup[p] = wtime[p][1] / np.array(wtime[p])   # Unused
-    effcy[p] = ((ncpus[p][1] * wtime[p][1])
-                    / (np.array(ncpus[p]) * np.array(wtime[p])))
-
     pcomm[p] = np.array(pcomm_raw)
+    msend[p] = np.array(msend_raw)
+    mreduce[p] = np.array(mreduce_raw)
+
 
 # Wall time across platforms
-fig, (ax_pc, ax_eff) = plt.subplots(2, 1, sharex=True, figsize=(7, 8))
+fig, (ax_pcomm, ax_call) = plt.subplots(2, 1, sharex=True, figsize=(7, 8))
 
 xticks = ncpus['12ppn']
 xticks = np.insert(xticks, 1, 160)
-ax_eff.set_xlabel('Number of CPUs')
+ax_call.set_xlabel('Number of CPUs')
 
-for ax in (ax_pc, ax_eff):
+for ax in (ax_pcomm, ax_call):
     # Disable default ticks
     ax.tick_params(axis='x',
                    which='minor',
@@ -61,27 +60,30 @@ for ax in (ax_pc, ax_eff):
 
 #ax_pc.set_ylim(0., 1.0)
 
-ax_pc.set_title('Model runtime')
-ax_pc.set_ylabel('Walltime (s)')
+#ax_pc.set_title('Model runtime')
+#ax_pc.set_ylabel('Walltime (s)')
 
-ax_eff.set_title('Scaling efficiency relative to 240 CPUs')
-ax_eff.set_ylabel('Scaling efficiency')
+#ax_eff.set_title('Scaling efficiency relative to 240 CPUs')
+#ax_eff.set_ylabel('Scaling efficiency')
 
-ax_eff.axhline(1.0, color='k', linestyle='--')
-ax_eff.axhline(0.8, color='k', linestyle=':', linewidth=0.5)
+#ax_eff.axhline(1.0, color='k', linestyle='--')
+#ax_eff.axhline(0.8, color='k', linestyle=':', linewidth=0.5)
 
-ax_data = {ax_pc: pcomm,
-           ax_eff: effcy}
-ax_lines = {ax_pc: [],
-            ax_eff: []}
+#ax_data = {ax_pc: pcomm,
+#           ax_eff: effcy}
+#ax_lines = {ax_pc: [],
+#            ax_eff: []}
 
-titles = ['Default', 'Hyperthr.', '12 PPN', 'Ladder', 'Snake']
-for ax in (ax_pc, ax_eff):
-    for p in ('raijin', 'ht', '12ppn', 'ladder', 'snake'):
-        ax_l, = ax.plot(ncpus[p], ax_data[ax][p], marker='+')
-        ax_lines[ax].append(ax_l)
+#titles = ['Default', 'Hyperthr.', '12 PPN', 'Ladder', 'Snake']
+#for ax in (ax_pc, ax_eff):
+#    for p in ('raijin', 'ht', '12ppn', 'ladder', 'snake'):
+#        ax_l, = ax.plot(ncpus[p], ax_data[ax][p], marker='+')
+#        ax_lines[ax].append(ax_l)
+#
+#    ax.legend(ax_lines[ax], titles, loc='best')
 
-    ax.legend(ax_lines[ax], titles, loc='best')
+for p in ('raijin', 'ht', '12ppn', 'ladder', 'snake'):
+    ax_pcomm.plot(ncpus[p], pcomm[p], marker='+')
 
 plt.tight_layout()
 #plt.savefig('scaling.pdf', bb_inches='tight')
